b0aa03950aa083fdb4d45c0a6404d38a7d78a285
HoangQuyen <hoangquyen@HoangQuyens-MacBook-Pro.local>
2023-12-30 23:39:59 +0700

    connect mongo atlas by MongoDb Driver

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,13 @@
 import express from 'express'
+import usersRouter from './routes/users.routes'
+import databaseService from './services/database.services'
+
 const app = express()
-const router = express.Router()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
-
-//middleware
-router.use((req, res, next) => {
-  console.log('Time', Date.now())
-  next()
-})
-
-router.get('/tweets', (req, res) => {
-  res.json({ data: [{ id: 1, text: 'hello world' }] })
-})
-
-app.use('/api', router)
+app.use(express.json())
+app.use('/users', usersRouter)
+databaseService.connect().catch(console.dir)
 
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`)
---
+++ src/services/database.services.ts
@@ -0,0 +1,30 @@
+import { MongoClient } from 'mongodb'
+import { config } from 'dotenv'
+
+config()
+const username = process.env.DB_USERNAME
+const password = process.env.DB_PASSWORD
+
+const uri = `mongodb+srv://${username}:${password}@twitter.cuvtkse.mongodb.net/?retryWrites=true&w=majority`
+
+class DatabaseService {
+  private client: MongoClient
+  constructor() {
+    this.client = new MongoClient(uri)
+  }
+
+  async connect() {
+    try {
+      // Send a ping to confirm a successful connection
+      await this.client.db('admin').command({ ping: 1 })
+      console.log('Pinged your deployment. You successfully connected to MongoDB!')
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await this.client.close()
+    }
+  }
+}
+
+// create an object from DatabaseService
+const databaseService = new DatabaseService()
+export default databaseService
