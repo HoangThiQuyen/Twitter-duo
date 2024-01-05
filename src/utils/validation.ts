e83b82e941d605c32396a4dd1791657416825e08
HoangQuyen <hoangquyen@HoangQuyens-MacBook-Pro.local>
2024-01-05 17:04:23 +0700

    synchronous types of errors

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from 'express'
 import { body, validationResult, ValidationChain } from 'express-validator'
 import { RunnableValidationChains } from 'express-validator/src/middlewares/schema'
+import HTTP_STATUS from '~/constants/httpStatus'
+import { EntityError, ErrorWithStatus } from '~/models/Errors'
 // can be reused by many routes
 
 // sequential processing, stops running validations chain if the previous one fails.
@@ -8,9 +10,20 @@ export const validate = (validation: RunnableValidationChains<ValidationChain>) 
   return async (req: Request, res: Response, next: NextFunction) => {
     await validation.run(req)
     const errors = validationResult(req)
+    // no error is next
     if (errors.isEmpty()) {
       return next()
     }
-    res.status(400).json({ errors: errors.mapped() })
+    const errorsObject = errors.mapped()
+    const entityErrors = new EntityError({ errors: {} })
+    for (const key in errorsObject) {
+      const { msg } = errorsObject[key]
+      // no errors from validate
+      if (msg instanceof ErrorWithStatus && msg.status !== HTTP_STATUS.UNPROCESSABLE_ENTITY) {
+        return next(msg)
+      }
+      entityErrors.errors[key] = errorsObject[key]
+    }
+    next(entityErrors)
   }
 }
