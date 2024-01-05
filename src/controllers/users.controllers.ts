54540a3ef4265ef0b82e904b2f4394df9c6c4181
HoangQuyen <hoangquyen@HoangQuyens-MacBook-Pro.local>
2024-01-05 15:11:57 +0700

    handle error with errorHandler

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,6 +1,4 @@
 import { Request, Response } from 'express'
-import User from '~/models/schemas/User.schema'
-import databaseService from '~/services/database.services'
 import usersService from '~/services/users.services'
 import { ParamsDictionary } from 'express-serve-static-core'
 import { RegisterRequestBody } from '~/models/requests/User.requests'
@@ -16,16 +14,9 @@ export const loginController = (req: Request, res: Response) => {
 }
 
 export const registerController = async (req: Request<ParamsDictionary, any, RegisterRequestBody>, res: Response) => {
-  try {
-    const result = await usersService.register(req.body)
-    res.json({
-      message: 'Register success',
-      result
-    })
-  } catch (error) {
-    return res.status(400).json({
-      message: 'Register failded',
-      error
-    })
-  }
+  const result = await usersService.register(req.body)
+  res.json({
+    message: 'Register success',
+    result
+  })
 }
