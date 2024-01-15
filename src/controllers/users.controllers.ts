65d4a940341f0b6d5bfe18267342082eb170678c
HoangQuyen <hoangquyen@HoangQuyens-MacBook-Pro.local>
2024-01-15 21:30:19 +0700

    hanlde logic login

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,22 +1,26 @@
 import { Request, Response } from 'express'
-import usersService from '~/services/users.services'
 import { ParamsDictionary } from 'express-serve-static-core'
-import { RegisterRequestBody } from '~/models/requests/User.requests'
+import { ObjectId } from 'mongodb'
 
-export const loginController = (req: Request, res: Response) => {
-  const { email, password } = req.body
-  if (email === 'quyen@gmail.com' && password === '123456') {
-    return res.json({ message: 'Login success' })
-  }
-  return res.status(400).json({
-    error: 'Login failed'
+import usersService from '~/services/users.services'
+import { RegisterRequestBody } from '~/models/requests/User.requests'
+import User from '~/models/schemas/User.schema'
+import { USERS_MESSAGES } from '~/constants/messages'
+
+export const loginController = async (req: Request, res: Response) => {
+  const user = req.user as User
+  const user_id = user._id as ObjectId
+  const result = await usersService.login(user_id.toString())
+  return res.json({
+    message: USERS_MESSAGES.LOGIN_SUCCESS,
+    result
   })
 }
 
 export const registerController = async (req: Request<ParamsDictionary, any, RegisterRequestBody>, res: Response) => {
   const result = await usersService.register(req.body)
   res.json({
-    message: 'Register success',
+    message: USERS_MESSAGES.REGISTER_SUCCESS,
     result
   })
 }
