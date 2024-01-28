0a067190593df191a04855bb71dab3a9303886ac
HoangQuyen <hoangquyen@HoangQuyens-MacBook-Pro.local>
2024-01-28 17:58:12 +0700

    completed updateMe api

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -10,6 +10,7 @@ import {
   RegisterRequestBody,
   ResetPasswordReqBody,
   TokenPayload,
+  UpdateMeReqBody,
   VerifyEmailReqBody,
   VerifyForgotPasswordReqBody
 } from '~/models/requests/User.requests'
@@ -126,6 +127,13 @@ export const getMeController = async (req: Request, res: Response, next: NextFun
   })
 }
 
-export const updateMeController = async (req: Request, res: Response, next: NextFunction) => {
-  return res.json({})
+export const updateMeController = async (
+  req: Request<ParamsDictionary, any, UpdateMeReqBody>,
+  res: Response,
+  next: NextFunction
+) => {
+  const { user_id } = req.decoded_authorization as TokenPayload
+  const { body } = req
+  const user = await usersService.updateMe(user_id, body)
+  return res.json({ message: USERS_MESSAGES.UPDATE_ME_SUCCESS, result: user })
 }
--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -1,6 +1,6 @@
 import User from '~/models/schemas/User.schema'
 import databaseService from './database.services'
-import { RegisterRequestBody } from '~/models/requests/User.requests'
+import { RegisterRequestBody, UpdateMeReqBody } from '~/models/requests/User.requests'
 import { hashPassword } from '~/utils/crypto'
 import { signToken } from '~/utils/jwt'
 import { TokenType, UserVerifyStatus } from '~/constants/enums'
@@ -203,6 +203,32 @@ class UsersService {
       { _id: new ObjectId(user_id) },
       {
         // remove properties unnecessary
+        projection: {
+          password: 0,
+          email_verify_token: 0,
+          forgot_password_token: 0
+        }
+      }
+    )
+    return user
+  }
+  async updateMe(user_id: string, payload: UpdateMeReqBody) {
+    const _payload = payload.date_of_birth ? { ...payload, date_of_birth: new Date(payload.date_of_birth) } : payload
+    // updateOne: Update but No return document for user
+    // findOneAndUpdate: Update and return document for user
+    const user = await databaseService.users.findOneAndUpdate(
+      { _id: new ObjectId(user_id) },
+      {
+        $set: {
+          ...(_payload as UpdateMeReqBody & { date_of_birth?: Date })
+        },
+        $currentDate: {
+          updated_at: true
+        }
+      },
+      {
+        // return document after update into mongodb
+        returnDocument: 'after',
         projection: {
           password: 0,
           email_verify_token: 0,
